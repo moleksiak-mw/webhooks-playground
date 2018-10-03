55858c89a62b2fb14d25fb05bfeee89b5bf102d7
Marcin Oleksiak <marcin.oleksiak@makingwaves.com>
2018-10-03 14:40:17 +0200

    added index.htm to gulp build

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,15 @@
 var gulp        = require('gulp');
 var deploy      = require('gulp-gh-pages');
 
+gulp.task('index', function () {
+	return gulp.src('./index.html')
+		.pipe(gulp.dest('dist'));
+});
+
 /**
  * Push build to gh-pages
  */
-gulp.task('deploy', function () {
+gulp.task('deploy', ['index'], function () {
   return gulp.src("./dist/**/*")
     .pipe(deploy())
 });
