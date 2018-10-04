3f5c7228c6b42bb35afc63d42b5bd609aa175fd0
Marcin Oleksiak <marcin.oleksiak@makingwaves.com>
2018-10-04 15:05:42 +0200

    dir change

--- gulpfile.js
+++ gulpfile.js
@@ -12,6 +12,6 @@ gulp.task('index', function () {
 gulp.task('deploy', ['index'], function () {
   return gulp.src("./dist/**/*")
     .pipe(deploy({
-		cacheDir: '.publish-gh-pages'
+		cacheDir: 'publish-gh-pages'
 	}))
 });
