a5f0cc2a5055a418a58cb7940a38c37eb7cf334b
Marcin Oleksiak <marcin.oleksiak@makingwaves.com>
2018-10-05 10:47:40 +0200

    aa

--- gulpfile.js
+++ gulpfile.js
@@ -3,15 +3,12 @@ var deploy      = require('gulp-gh-pages');
 
 gulp.task('index', function () {
 	return gulp.src('./index.html')
-		.pipe(gulp.dest('dist'));
+		.pipe(gulp.dest('deploy'));
 });
 
 /**
  * Push build to gh-pages
  */
 gulp.task('deploy', ['index'], function () {
-  return gulp.src("./dist/**/*")
-    .pipe(deploy({
-		cacheDir: 'publish-gh-pages'
-	}))
+  return gulp.src("./deploy/**/*");
 });
